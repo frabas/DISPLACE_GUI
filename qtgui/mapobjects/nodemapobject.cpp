a995bbaee354253fed2f17d7467b258b5c008e61
Federico Fuga <fuga@studiofuga.com>
2014-09-26 16:13:52 +0200

    gui: Nodes color based on landscape value

--- qtgui/mapobjects/nodemapobject.cpp
+++ qtgui/mapobjects/nodemapobject.cpp
@@ -9,4 +9,8 @@ NodeMapObject::NodeMapObject(Node *node)
                                                                           mNode->get_x(), mNode->get_y()
                                                                           )
                                                                       ));
+
+   int l = mNode->get_marine_landscape();
+   QColor c = QColor(((l / 100) % 10) * 25, ((l / 10) % 10) * 25, (l % 10 )* 25);
+   mGeometry->setBrush(c);
 }

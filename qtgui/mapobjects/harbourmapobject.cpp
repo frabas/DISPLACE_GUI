e8453a691fad8a3e2de03ea084b4d8c4cd9c44a2
Federico Fuga <fuga@studiofuga.com>
2014-10-20 18:01:55 +0200

    Gui: Harbour popup stats

--- qtgui/mapobjects/harbourmapobject.cpp
+++ qtgui/mapobjects/harbourmapobject.cpp
@@ -1,10 +1,16 @@
 #include "harbourmapobject.h"
 
+#include <mapobjectscontroller.h>
+
+#include <QMapControl/QMapControl.h>
+
 QPixmap *HarbourMapObject::symbol = 0;
 
-HarbourMapObject::HarbourMapObject(HarbourData *harbour)
-    : mHarbour(harbour),
-      mGeometry()
+HarbourMapObject::HarbourMapObject(MapObjectsController *controller, HarbourData *harbour)
+    : mController(controller),
+      mHarbour(harbour),
+      mGeometry(),
+      mWidget(0)
 {
     if (symbol == 0)
         symbol = new QPixmap(":/icons/harbour.png");
@@ -17,4 +23,46 @@ HarbourMapObject::HarbourMapObject(HarbourData *harbour)
 
     mGeometry->setMetadata(std::string("name"), QString::fromStdString(mHarbour->mHarbour->get_name()));
     mGeometry->setMetadataDisplayed(std::string("name"));
+
+    mGeometry->setAncillaryData(new MapObjectsController::WidgetAncillaryData(this));
+}
+
+bool HarbourMapObject::clicked()
+{
+    if (!mWidget) {
+        mWidget = new NodeDetailsWidget(mController->mapWidget());
+        connect (mWidget, SIGNAL(destroyed()), this, SLOT(widgetClosed()));
+    }
+
+    update();
+    mController->showDetailsWidget(mGeometry->coord(),mWidget);
+
+    return true;
+}
+
+void HarbourMapObject::update()
+{
+    if (!mWidget)
+        return;
+
+    QString text = QString("<b>Name</b>: %1<br/>"
+                           "<b>Coords: </b>%2 %3<br/>")
+            .arg(QString::fromStdString(mHarbour->mHarbour->get_name()))
+            .arg(mHarbour->mHarbour->get_y())
+            .arg(mHarbour->mHarbour->get_x());
+
+    text += "<br/>";
+    /*
+    text += QString("<b>Fuel:</b> %1<br/>").arg(mVessel->mVessel->get_cumfuelcons());
+    text += QString("<b>State:</b> %1<br/>").arg(mVessel->mVessel->get_state());
+    text += QString("<b>Cum Catches:</b> %1<br/>").arg(mVessel->mVessel->get_cumcatches());
+    text += QString("<b>Time at sea:</b> %1<br/>").arg(mVessel->mVessel->get_timeatsea());
+    text += QString("<b>Reason To go Back:</b> %1<br/>").arg(mVessel->mVessel->get_reason_to_go_back());
+*/
+    mWidget->setText(text);
+}
+
+void HarbourMapObject::widgetClosed()
+{
+    mWidget = 0;
 }
--- qtgui/mapobjects/harbourmapobject.h
+++ qtgui/mapobjects/harbourmapobject.h
@@ -1,25 +1,42 @@
 #ifndef HARBOURMAPOBJECT_H
 #define HARBOURMAPOBJECT_H
 
+#include <QObject>
+
 #include <mapobjects/mapobject.h>
 #include <QMapControl/GeometryPointImageScaled.h>
 #include <modelobjects/harbourdata.h>
+#include <mapobjects/nodedetailswidget.h>
+
 #include <QPixmap>
 
-class HarbourMapObject : public MapObject
+class MapObjectsController;
+
+class HarbourMapObject : public QObject, public MapObject
 {
+    Q_OBJECT
 public:
-    HarbourMapObject(HarbourData *harbour);
+    HarbourMapObject(MapObjectsController *controller, HarbourData *harbour);
 
     std::shared_ptr<qmapcontrol::Geometry> getGeometryEntity() const {
         return mGeometry;
     }
 
+    virtual bool clicked();
+    virtual void update();
+
+private slots:
+    void widgetClosed();
+
 private:
+    MapObjectsController *mController;
+
     HarbourData *mHarbour;
     std::shared_ptr<qmapcontrol::GeometryPointImageScaled> mGeometry;
 
     static QPixmap *symbol;
+    NodeDetailsWidget *mWidget;
+
 };
 
 #endif // HARBOURMAPOBJECT_H

a585178b362d948d0849a6c396e9b6bd2033e373
Federico Fuga <fuga@studiofuga.com>
2014-11-11 13:09:32 +0100

    Fix calendar loading mistake.

--- qtgui/calendar.cpp
+++ qtgui/calendar.cpp
@@ -17,15 +17,15 @@ Calendar *Calendar::load(QString basepath, QString name)
 
     std::vector<int> quarters = read_tsteps_quarters(name.toStdString(), basepath.toStdString());
     for(size_t i = 0; i < quarters.size(); ++i)
-        calendar->mQuarters.insert(months[i], i);
+        calendar->mQuarters.insert(quarters[i], i);
 
     std::vector<int> sems = read_tsteps_semesters(name.toStdString(), basepath.toStdString());
     for(size_t i = 0; i < sems.size(); ++i)
-        calendar->mSemesters.insert(months[i], i);
+        calendar->mSemesters.insert(sems[i], i);
 
     std::vector<int> yrs = read_tsteps_years(name.toStdString(), basepath.toStdString());
     for(size_t i = 0; i < yrs.size(); ++i)
-        calendar->mYears.insert(months[i], i);
+        calendar->mYears.insert(yrs[i], i);
 
     Q_ASSERT(calendar->mMonths.size() > 0);
     Q_ASSERT(calendar->mQuarters.size() > 0);

cdb98b9c0183d3900ada78b0529ec539361c22f8
Francois Bastardie <fba@aqua.dtu.dk>
2015-06-01 12:08:26 +0200

    simulator issue #120: include new input file for metier stock targets

--- include/Metier.h
+++ include/Metier.h
@@ -44,21 +44,22 @@ class Metier
 		double gear_width_b;
 		string gear_width_model;
 		multimap<int, double> loss_after_1_passage;
+        multimap<int, int> metier_target_stocks;
 
 	public:
 		Metier();
-		Metier(int name,
-			int type,
-			vector<double> selectivity,
-			vector<double> discards,
-			vector<double> metier_betas_per_pop,
+        Metier(int name,
+            int type,
+            vector<double> selectivity,
+            vector<double> discards,
+            vector<double> metier_betas_per_pop,
             vector<int> metier_mls_cat_per_pop,
             double fspeed,
             double gear_width_a,
-			double gear_width_b,
-			string gear_width_model,
-			multimap<int, double> loss_after_1_passage
-			);
+            double gear_width_b,
+            string gear_width_model,
+            multimap<int, double> loss_after_1_passage,
+            multimap<int, int> _metier_target_stocks);
 		~Metier();
 		int get_name();
 		int get_metier_type();
@@ -70,7 +71,8 @@ class Metier
 		double  get_gear_width_b();
 		string  get_gear_width_model();
 		multimap<int, double>  get_loss_after_1_passage();
-		vector<double> get_betas_per_pop();
+        multimap<int, int>  get_metier_target_stocks();
+        vector<double> get_betas_per_pop();
         vector<int> get_mls_cat_per_pop();
         void set_betas_per_pop(vector<double> metier_betas_per_pop);
         void set_mls_cat_per_pop(vector<int> metier_mls_cat_per_pop);

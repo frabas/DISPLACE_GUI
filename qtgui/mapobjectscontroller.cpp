df87d08b449de5e2fc93da22123fee208f69e8c9
ing. Federico Fuga <fuga@studiofuga.com>
2015-06-18 18:05:52 +0200

    Nodes/Edges layer order exchanged in map.

--- qtgui/mapobjectscontroller.cpp
+++ qtgui/mapobjectscontroller.cpp
@@ -79,8 +79,8 @@ void MapObjectsController::createMapObjectsFromModel(int model_n, DisplaceModel 
     mEdgesLayer[model_n] = std::shared_ptr<EdgeLayer>(new EdgeLayer(this, QString(tr("#%1#Graph Edges")).arg(model_n)));
 
     addStandardLayer(model_n, LayerEntities, mEntityLayer[model_n], true);
-    addStandardLayer(model_n, LayerGraph, mGraphLayer[model_n], true);
     addStandardLayer(model_n, LayerEdges, mEdgesLayer[model_n]->layer(), false);
+    addStandardLayer(model_n, LayerGraph, mGraphLayer[model_n], true);
 
     mStatsLayerPop[model_n] = std::shared_ptr<qmapcontrol::LayerGeometry>(new qmapcontrol::LayerGeometry(QString(tr("#%1#Abundance")).arg(model_n).toStdString()));
     addOutputLayer(model_n, OutLayerPopStats, mStatsLayerPop[model_n], type != DisplaceModel::LiveModelType ? false : true);

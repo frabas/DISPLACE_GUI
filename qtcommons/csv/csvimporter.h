1d700a87e79b47986afb606af21381d9ab873300
ing. Federico Fuga <fuga@studiofuga.com>
2015-06-24 18:24:35 +0200

    Fixes to Exceptions in csv Import/Export classes

--- qtcommons/csv/csvimporter.h
+++ qtcommons/csv/csvimporter.h
@@ -18,13 +18,17 @@ public:
         void raise() const { throw *this; }
         Exception *clone() const { return new Exception(*this); }
 
+        const char *what() const _GLIBCXX_USE_NOEXCEPT {
+            return mWhat.toStdString().c_str();
+        }
+
     protected:
         QString mWhat;
     };
 
     CsvImporter();
 
-    QList<QStringList> import (QString filename);
+    QList<QStringList> import (QString filename) throw (Exception);
 
     void setSeparator (QChar sep) { mSeparator = sep; }
 protected:

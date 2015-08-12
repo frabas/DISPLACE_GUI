frabas/DISPLACE_GUI

commit dca787a844748c99067fac3e7e8467eeb40d98fc

Fixes to compile simulator without Qt and without c++11 support.

commons/dtree/variables.cpp
@@ -1,6 +1,6 @@
 #include <dtree/variables.h>
 
-using namespace dtree ;
+using namespace dtree;
 
 const int VariableNames::VERSION = 6;
 
@@ -50,8 +50,7 @@ const char *const VariableNames::names[] = {
     0
 };
 
-#if ((__cplusplus >= 201103L))
-const std::vector<std::string> VariableNames::bins[] = {
+const char *const VariableNames::bins[][30] = {
     { "0", "1","2","3","4","5","6","7","8","9",
       "10", "11","12","13","14","15","16","17","18","19",
       "20", "21","22","23","24","25","26","27","28","29"  },
@@ -93,7 +92,6 @@ const std::vector<std::string> VariableNames::bins[] = {
     { },  // probability
     // keep this last
 };
-#endif
 
 const char *VariableNames::variableName(Variable id)
 {
@@ -113,4 +111,11 @@ Variable VariableNames::variableCode(const std::string &name)
     }
 
     return VarUndefined;
+}
+
+int VariableNames::variableBinCount(Variable var)
+{
+    int i = 0;
+    for (i = 0; bins[var][i] != 0; ++i);
+    return i;
 }

commons/dtree/variables.h
@@ -56,17 +56,13 @@ public:
     static const char *variableName(Variable);
     static Variable variableCode (const std::string &name);
 
-#if ((__cplusplus >= 201103L))
-    static int variableBinCount(Variable var) { return bins[var].size(); }
-    static const char *variableBin(Variable var, int ndx) { return bins[var][ndx].c_str(); }
-#endif
+    static int variableBinCount(Variable var);
+    static const char *variableBin(Variable var, int ndx) { return bins[var][ndx]; }
 
 private:
     static const char *const names[];
 
-#if ((__cplusplus >= 201103L))
-    static const std::vector<std::string> bins[];
-#endif
+    static const char *const bins[][30];
 };
 
 }

commons/Makefile
@@ -1,17 +1,3 @@
-TEMPLATE= lib
-TARGET=displacecommons
-DESTDIR=../
-CONFIG += c++11
-
-include ("$$top_srcdir/localconfig.pri")
-
-INCLUDEPATH +=../include/ .
-
-win32 {
-    # For GetProcessMemoryInfo()
-    LIBS += -lpsapi
-}
-
 SOURCES= \
     readdata.cpp \
     myutils.cpp \
@@ -74,9 +60,17 @@ HEADERS= \
     tseries/timeseriesmanager.h \
     dtree/evaluators/timeseriesevaluator.h
 
-OTHER_FILES += \
-    Makefile
+LIBS=-lpthread -lrt
+CPPFLAGS=-O2 -fpermissive -I../include -I../commons -I. -fpic
+LDFLAGS=
 
-target.path = $${PREFIX}/lib
-headers.path = $${PREFIX}/include/
-headers.files = $${HEADERS}
+OBJS=$(SOURCES:.cpp=.o)
+
+libcommons.so: ${OBJS}
+	${CXX} -shared ${LDFLAGS} -o $@ $^ ${LIBS}
+	
+	
+	
+clean:
+	@-rm ${OBJS} libcommons.so
+

commons/Vessel.cpp
@@ -36,6 +36,7 @@
 #include <dtree/externalstatemanager.h>
 #include <dtree/commonstateevaluators.h>
 #include <dtree/evaluators/timeseriesevaluator.h>
+#include <dtree/variables.h>
 
 #include <boost/make_shared.hpp>
 
@@ -271,19 +272,20 @@ void Vessel::init()
 
     // Lazy initialization of the global State Evaluator.
     if (mStateEvaluators.size() == 0) {
-        for (int i = 0; i < dtree::Variable::VarLast; ++i) {
+        for (int i = 0; i < dtree::VarLast; ++i) {
             mStateEvaluators.push_back(boost::shared_ptr<dtree::StateEvaluator>());
         }
 
         // Add here the variables associations
-        mStateEvaluators[dtree::vesselSizeIs] = boost::make_shared<dtree::VariableReferenceStateEvaluator<LengthClass> >(mLengthClassId);
+        mStateEvaluators[dtree::vesselSizeIs] =
+                boost::shared_ptr<dtree::StateEvaluator> (new dtree::VariableReferenceStateEvaluator<LengthClass>(mLengthClassId));
         mStateEvaluators[dtree::lastTripRevenueIs] = boost::shared_ptr<dtree::StateEvaluator>(new dtree::TwoArgumentsComparatorStateEvaluator<std::less<double> >(
-                    boost::make_shared<dtree::VariableReferenceStateEvaluator<double> >(lastTrip_revenues),
-                    boost::make_shared<dtree::vessels::AverageRevenuesStateEvaluator>(),
+                    boost::shared_ptr<dtree::StateEvaluator> (new dtree::VariableReferenceStateEvaluator<double>(lastTrip_revenues)),
+                    boost::shared_ptr<dtree::StateEvaluator> (new dtree::vessels::AverageRevenuesStateEvaluator()),
                     std::less<double>()));
         mStateEvaluators[dtree::lastTripProfitIs] = boost::shared_ptr<dtree::StateEvaluator>(new dtree::TwoArgumentsComparatorStateEvaluator<std::less<double> >(
-                    boost::make_shared<dtree::VariableReferenceStateEvaluator<double> >(lastTrip_profit),
-                    boost::make_shared<dtree::vessels::AverageProfitStateEvaluator>(),
+                    boost::shared_ptr<dtree::StateEvaluator> (new dtree::VariableReferenceStateEvaluator<double>(lastTrip_profit)),
+                    boost::shared_ptr<dtree::StateEvaluator> (new dtree::vessels::AverageProfitStateEvaluator()),
                     std::less<double>()));
         mStateEvaluators[dtree::fuelPriceIs] = boost::shared_ptr<dtree::StateEvaluator>(new displace::dtree::TimeSeriesEvaluator<displace::simulation::TimeSeriesManager::Fuelprice>());
         mStateEvaluators[dtree::fishPriceTargetStockIs] = boost::shared_ptr<dtree::StateEvaluator>(new displace::dtree::TimeSeriesEvaluator<displace::simulation::TimeSeriesManager::Fishprice>());

commons/dtree/decisiontreemanager.cpp
@@ -85,6 +85,15 @@ void DecisionTreeManager::readScenarioFileIfAvailable(std::string path, std::str
     }
 }
 
+struct NodePrototype {
+    int id;
+    Variable variable;
+    double value;
+    std::vector<int> children;
+    std::vector<int> mapping;
+    boost::shared_ptr<dtree::Node> node;
+};
+
 /** \brief Loads a specific file, creating the dtree and allocating it to the type specified in the file
  * */
 bool DecisionTreeManager::readFile (std::string filename)
@@ -92,21 +101,13 @@ bool DecisionTreeManager::readFile (std::string filename)
     std::cout << "@DEBUG: Reading file " << filename << std::endl;
 
     std::ifstream stream;
-    stream.open(filename, std::ios_base::in);
+    stream.open(filename.c_str(), std::ios_base::in);
     if (stream.fail()) {
         return false;
     }
 
     boost::shared_ptr<dtree::DecisionTree> tree (new dtree::DecisionTree());
 
-    struct NodePrototype {
-        int id;
-        dtree::Variable variable;
-        double value;
-        std::vector<int> children;
-        std::vector<int> mapping;
-        boost::shared_ptr<dtree::Node> node;
-    };
     std::vector<NodePrototype> nodes;
 
     DecisionTreeManager::TreeType treeType = DecisionTreeManager::InvalidTreeType;
@@ -136,7 +137,7 @@ bool DecisionTreeManager::readFile (std::string filename)
 
             NodePrototype prt;
             prt.id = atoi(fields[0].c_str());
-            prt.variable = dtree::VariableNames::variableCode(fields[1]);
+            prt.variable = VariableNames::variableCode(fields[1]);
             // fields 2 and 3 are ignored (posx,posy)
             int n = atoi(fields[4].c_str());
             int fldnum = 5;

commons/dtree/externalstatemanager.cpp
@@ -4,7 +4,7 @@ ExternalStateManager *ExternalStateManager::sInstance = 0;
 
 ExternalStateManager::ExternalStateManager()
 {
-    for (int i = 0; i < dtree::Variable::VarLast; ++i)
+    for (int i = 0; i < dtree::VarLast; ++i)
         mStandardEvaluators.push_back(0);
 
     // build evaluators

simulator/Makefile
@@ -49,11 +49,16 @@ HEADERS= \
         ../include/memoryinfo.h \
         ../include/options.h
 
-LIBS=-lpthread -lrt
-CPPFLAGS=-O2 -fpermissive -I../include -I.
+LIBS=-L../commons -lcommons -lpthread -lrt
+CPPFLAGS=-O2 -fpermissive -I../include -I../commons -I.
 LDFLAGS=
 
 OBJS=$(SOURCES:.cpp=.o)
 
 displace: ${OBJS}
 	${CXX} ${LDFLAGS} -o $@ $^ ${LIBS}
+	
+	
+clean:
+	-@rm ${OBJS} displace
+

commit 1501ca4e8b7018e72073032e584c917096624827

Fix bug introduced with commit dca787a that breaks vesselMetier dtree.

commons/dtree/variables.cpp
@@ -50,46 +50,46 @@ const char *const VariableNames::names[] = {
     0
 };
 
-const char *const VariableNames::bins[][30] = {
+const char *const VariableNames::bins[][40] = {
     { "0", "1","2","3","4","5","6","7","8","9",
       "10", "11","12","13","14","15","16","17","18","19",
-      "20", "21","22","23","24","25","26","27","28","29"  },
-    { "u15m", "15-18m", "18-24m", "24-40m", "o40m"},    // given by length_class in Vessel::Vessel()
-    { "below av.", "above av." },   //lastTripProfitIs
-    {  "below av.", "above av."},      //lastTripRevenueIs
-    { "low", "medium", "large" }, // or { "<5knots", "5-10knots", ">10knots" },
-    { "low","medium", "high" },
-    { "low", "medium", "high" },  // fishPriceTargetStockIs a difficult one which requires an extra info to link metier names to targetted stock(s)
-    { "low", "high" },
-    { "ongoing", "ending" },
+      "20", "21","22","23","24","25","26","27","28","29", 0  },
+    { "u15m", "15-18m", "18-24m", "24-40m", "o40m", 0},    // given by length_class in Vessel::Vessel()
+    { "below av.", "above av.", 0 },   //lastTripProfitIs
+    {  "below av.", "above av.",0 },      //lastTripRevenueIs
+    { "low", "medium", "large",0 }, // or { "<5knots", "5-10knots", ">10knots" },
+    { "low","medium", "high",0 },
+    { "low", "medium", "high",0 },  // fishPriceTargetStockIs a difficult one which requires an extra info to link metier names to targetted stock(s)
+    { "low", "high",0 },
+    { "ongoing", "ending",0 },
 
 
-    { "not expected", "expected" },
-    { "bad", "good"},
-    { "bad", "good"},
-    { "low", "high" },
+    { "not expected", "expected",0 },
+    { "bad", "good",0 },
+    { "bad", "good",0 },
+    { "low", "high",0 },
 
-    { "false", "true" },
-    { "false", "true" },
-    { "false", "true" },
-//    { "low", "high" },
-    { "no", "yes" },
+    { "false", "true",0 },
+    { "false", "true",0 },
+    { "false", "true",0 },
+//    { "low", "high",0 },
+    { "no", "yes",0 },
 
-    { "after 3 h", "after 6 h" },
-    { "no", "yes" },
+    { "after 3 h", "after 6 h",0 },
+    { "no", "yes",0 },
 
-    { "still ok", "fulfilled" },
-    { "still ok", "low" },
-    { "true", "false" },
-    { ">5", ">14" },  //or  {"short", "long" },
-    { "bad", "good" },
+    { "still ok", "fulfilled",0 },
+    { "still ok", "low",0 },
+    { "true", "false",0 },
+    { ">5", ">14",0 },  //or  {"short", "long" },
+    { "bad", "good",0 },
 
-    { "<10nm", "<100nm", ">100nm" },  // or { "near", "not that far", "far" },
-    { "low", "high" },
-    { "low", "high" },
-    { "not mine", "mine" },
+    { "<10nm", "<100nm", ">100nm",0 },  // or { "near", "not that far", "far" },
+    { "low", "high",0 },
+    { "low", "high",0 },
+    { "not mine", "mine",0 },
 
-    { },  // probability
+    { 0 },  // probability
     // keep this last
 };
 

commons/dtree/variables.h
@@ -62,7 +62,7 @@ public:
 private:
     static const char *const names[];
 
-    static const char *const bins[][30];
+    static const char *const bins[][40];
 };
 
 }
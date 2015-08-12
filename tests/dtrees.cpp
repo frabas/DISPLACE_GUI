406e675fb22edd6691aee35fd23d46ab52e9d97a
Federico Fuga <fuga@studiofuga.com>
2015-08-12 15:55:44 +0200

    [UnitTest] sample unit test fixed

--- tests/dtrees.cpp
+++ tests/dtrees.cpp
@@ -4,18 +4,13 @@
 #include <dtree/decisiontreemanager.h>
 #include <boost/test/unit_test.hpp>
 
-struct hello_world_fixture
-{
-    std::ostringstream dest;
-};
-
-BOOST_FIXTURE_TEST_SUITE(test_hello_world, hello_world_fixture);
+BOOST_AUTO_TEST_SUITE(Dtree)
 
 BOOST_AUTO_TEST_CASE( Dtree )
 {
     dtree::DecisionTreeManager *manager = dtree::DecisionTreeManager::manager();
 
-    BOOST_CHECK( manager->hasTree(dtree::DecisionTreeManager::GoFishing) != false );
+    BOOST_CHECK( manager->hasTree(dtree::DecisionTreeManager::GoFishing) == false );
 }
 
 
--- tests/tests.pro
+++ tests/tests.pro
@@ -1,11 +1,12 @@
 QT=
 CONFIG=
+TARGET=unittest
 
 DESTDIR = ../
 
 DEFINES=
 INCLUDEPATH=../include ../commons
-LIBS=-L.. -ldisplacecommons -lcommons -lboost_unit_test_framework
+LIBS=-L.. -ldisplacecommons -lpthread -lrt -lboost_unit_test_framework
 
 SOURCES= main.cpp \
     dtrees.cpp

d116e64bcb793c78cd9374efe714d9a21c4c3c20
Francois Bastardie <fba@aqua.dtu.dk>
2016-01-19 10:58:24 +0100

    v0.8.1.1

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.8.1"
+#define VERSION "0.8.1.1"
 #define VERSION_BUILD 0
 #define VERSION_COMMA 0,8,1,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "0.8.1\0"
+#define VER_PRODUCTVERSION_STR      "0.8.1.1\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"

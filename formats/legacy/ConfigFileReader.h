e461fd400fcdcdb2a0fa2530657eae68480308e5
Federico Fuga <fuga@studiofuga.com>
2016-03-03 18:26:37 +0100

    New subproject for library "formats"

---
+++ formats/legacy/ConfigFileReader.h
@@ -0,0 +1,22 @@
+//
+// Created by Federico Fuga on 03/03/16.
+//
+
+#ifndef DISPLACE_CONFIGFILEREADER_H
+#define DISPLACE_CONFIGFILEREADER_H
+
+namespace displace {
+    namespace formats {
+
+        namespace legacy {
+
+            class ConfigFileReader {
+
+            };
+
+        }
+
+    }
+}
+
+#endif //DISPLACE_CONFIGFILEREADER_H

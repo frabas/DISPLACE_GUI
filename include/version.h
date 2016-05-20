515418f206741ae8bac9ffad21671c1184a180ed
Federico Fuga <fuga@studiofuga.com>
2016-05-20 17:45:13 +0200

    New Version 0.8.6

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.8.5"
+#define VERSION "0.8.6"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 0,8,5,VERSION_BUILD
+#define VERSION_COMMA 0,8,6,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "0.8.5\0"
+#define VER_PRODUCTVERSION_STR      "0.8.6\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"

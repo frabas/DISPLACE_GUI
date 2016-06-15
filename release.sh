b3b5bfaa7cc11f312ea58b4fc39d3cac86d3590c
ing. Federico Fuga <fuga@studiofuga.com>
2016-06-15 17:06:41 +0200

    Linux Release script updated

--- release.sh
+++ release.sh
@@ -2,13 +2,39 @@
 
 set -e
 
+help() {
+cat << _EOF
+Usage: release [-v version] [-h] [suffix]
+
+	-v version	Define version string. If not used,
+			version is desumed from git tags
+	-h		Show this help
+	suffix		Append suffix to version string
+_EOF
+}
+
 VERSION=`git describe --abbrev=0 --always --tags`
-NAME=displace-$VERSION
+
+case "$1" in
+	-v) 
+		shift
+		VERSION="$1"
+		shift
+		;;
+	-h)
+		help
+		exit 1
+		;;
+esac
+
+MOD="$1"
+
+NAME=displace-$VERSION$MOD
 TEMP=../temp-src
 SRCPATH=$TEMP/$NAME
 
 mkdir -p $SRCPATH
-rsync -rav --progress --exclude .git --exclude .qt.conf --exclude \*~ --exclude \*.tar.gz --exclude build . $SRCPATH
+rsync -rav --progress --exclude .git --exclude .qt.conf --exclude \*~ --exclude \*.tar.gz --exclude build --exclude 3rd-party . $SRCPATH
 ( cd $TEMP && tar zcvf $NAME.tar.gz $NAME )
 
 mv $TEMP/$NAME.tar.gz .

2194856ec325468cb6e921922f71cf5b6144426c
ing. Federico Fuga <fuga@studiofuga.com>
2016-09-13 16:56:18 +0200

    R Script settings class updated

--- qtcommons/R/settings.cpp
+++ qtcommons/R/settings.cpp
@@ -3,8 +3,25 @@
 
 using namespace displace::R;
 
+const char *const Settings::BasePathKey = "R::basePath";
+
+QString Settings::getScriptBasePath() const
+{
+    return mSettings.value(BasePathKey, defaults::getRScriptsPath()).toString();
+}
+
 QString Settings::getScriptPath(QString scriptName) const
 {
-    return mSettings.value("R::" + scriptName, defaults::getRScriptsPath() + "/" + scriptName).toString();
+    return mSettings.value("R::" + scriptName, getScriptBasePath() + "/" + scriptName).toString();
+}
+
+void Settings::setScriptBasePath(const QString &path)
+{
+    mSettings.setValue(BasePathKey, path);
+}
+
+void Settings::setScriptPath(const QString &scriptName, const QString &path)
+{
+    mSettings.setValue("R::" + scriptName, path);
 }
 
--- qtcommons/R/settings.h
+++ qtcommons/R/settings.h
@@ -10,9 +10,14 @@ namespace R {
 class Settings
 {
     QSettings mSettings;
+    static const char *const BasePathKey;
 
 public:
+    QString getScriptBasePath() const;
     QString getScriptPath(QString scriptName) const;
+
+    void setScriptBasePath(const QString &path);
+    void setScriptPath (const QString &scriptName, const QString &path);
 };
 
 }

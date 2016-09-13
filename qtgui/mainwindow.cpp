bada1bd4693909d1cb471e395db643402c3098a0
Francois Bastardie <fba@aqua.dtu.dk>
2016-09-13 08:35:38 +0200

    Fix for calling the right vessel editor ui i.e. vsleditor

--- qtgui/mainwindow.cpp
+++ qtgui/mainwindow.cpp
@@ -2355,7 +2355,7 @@ void MainWindow::on_actionTime_Series_Editor_triggered()
 
 void MainWindow::on_actionVessel_Creation_Editor_triggered()
 {
-    auto ed = prepareAppExecutableStart("vesseleditor");
+    auto ed = prepareAppExecutableStart("vsleditor");
     ed->start();
 }
 

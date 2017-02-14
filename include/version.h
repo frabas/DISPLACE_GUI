dd857fb06ffe18ec9cdb6d25f78752ce6105020c
Francois Bastardie <fba@aqua.dtu.dk>
2017-02-14 15:36:05 +0100

    v0.9.5

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.9.4"
+#define VERSION "0.9.5"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 0,9,4,VERSION_BUILD
+#define VERSION_COMMA 0,9,5,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "0.9.4\0"
+#define VER_PRODUCTVERSION_STR      "0.9.5\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"

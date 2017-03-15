33b3e4ca8de5e4f3f76bb68e1e13b0c3e2b5c84a
Francois Bastardie <fba@aqua.dtu.dk>
2017-03-15 15:14:13 +0100

    0.9.7 (amending bug in benthos dyn in 0.9.6)

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.9.6"
+#define VERSION "0.9.7"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 0,9,6,VERSION_BUILD
+#define VERSION_COMMA 0,9,7,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "0.9.6\0"
+#define VER_PRODUCTVERSION_STR      "0.9.7\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"

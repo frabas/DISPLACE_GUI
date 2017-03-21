4f0910093dfdc1ebbfe2b708d891162990890751
ing. Federico Fuga <fuga@studiofuga.com>
2017-03-21 17:16:02 +0100

    Allow memtest to run valgrind massif

--- memtest.sh
+++ memtest.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+help() {
+  cat << __EOF
+Usage: memtest.sh [-m] [-p path] [-h] [MOD] [MODEL]
+
+  -m          Run massif
+  -p path     Run an alternative binary (build/release-qt)
+  -h          Show this help
+
+  MOD         The mod (testexample)
+  MODEL       The config (baseline)
+__EOF
+}
+
+
+SRC=`pwd`
 MYPATH=`pwd`/build/release-qt5/
 MOD=testexample
 MODEL=baseline
@@ -7,6 +22,26 @@ SIMU=simu2
 STEPS=8762
 
 export LD_LIBRARY_PATH=${MYPATH}
+
+while [ "$1" != "" ] ; do
+  case "$1" in
+    -m)
+      RUN="valgrind --tool=massif --massif-out-file=${SRC}/massive.out"
+      shift
+      ;;
+    -p)
+      shift
+      MYPATH=`realpath $1`
+      shift
+      ;;
+    -h)
+      help
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
 
 if [ "$1" != "" ] ; then
   MOD=$1
@@ -18,5 +53,5 @@ fi
 SIM=/home/happycactus/Documents/Progetti/displace/Applications/DISPLACE_input_${MOD}
 
 cd $SIM
-${MYPATH}/displace -f ${MOD} -f2 ${MODEL} -s ${SIMU} -i ${STEPS} -p 1 -o 1 -e 1 -v 0 --without-gnuplot -V 1 --num_threads 1
+${RUN} ${MYPATH}/displace -f ${MOD} -f2 ${MODEL} -s ${SIMU} -i ${STEPS} -p 1 -o 1 -e 1 -v 0 --without-gnuplot -V 1 --num_threads 1
 

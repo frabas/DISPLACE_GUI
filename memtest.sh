55c81119a63cbcf7e37b731587ee7bd4fe96e51a
ing. Federico Fuga <fuga@studiofuga.com>
2017-03-21 13:57:18 +0100

    Memtest script fixed

--- memtest.sh
+++ memtest.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-PATH=`pwd`/build/release-qt5/
+MYPATH=`pwd`/build/release-qt5/
 MOD=testexample
 MODEL=baseline
 SIMU=simu2
 STEPS=8762
 
-LD_LIBRARY_PATH=${PATH}
+export LD_LIBRARY_PATH=${MYPATH}
 
 if [ "$1" != "" ] ; then
   MOD=$1
@@ -18,5 +18,5 @@ fi
 SIM=/home/happycactus/Documents/Progetti/displace/Applications/DISPLACE_input_${MOD}
 
 cd $SIM
-${PATH}/displace -f ${MOD} -f2 ${MODEL} -s ${SIMU} -i ${STEPS} -p 1 -o 1 -e 1 -v 0 --without-gnuplot -V 1 --num_threads 1
+${MYPATH}/displace -f ${MOD} -f2 ${MODEL} -s ${SIMU} -i ${STEPS} -p 1 -o 1 -e 1 -v 0 --without-gnuplot -V 1 --num_threads 1
 

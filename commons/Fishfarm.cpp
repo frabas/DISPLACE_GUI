8a54a528074a8482f8c4e3b412cee05bead17f6d
Francois Bastardie <fba@aqua.dtu.dk>
2017-05-22 12:12:33 +0200

    Getters and setters for fishfarm new attributes

--- commons/Fishfarm.cpp
+++ commons/Fishfarm.cpp
@@ -30,7 +30,8 @@
 
 Fishfarm::Fishfarm(int _name, string _stringname, Node *_node, double _size, double _farm_original_long, double _farm_original_lat,
                    double _mean_SST, double _mean_salinity, double _mean_windspeed, double _mean_currentspeed, double _max_depth, double _diss_O2_mg_per_l,
-                   double _Linf_mm, double _K_y, double _t0_y, double _fulton_condition_factor, string _meanw_growth_model_type,
+                   double _Linf_mm, double _K_y, double _t0_y, double _fulton_condition_factor,
+                   string _meanw_growth_model_type,
                    int _start_day_growing, int _end_day_harvest, int _nb_days_fallowing_period,
                    int _nb_fish_at_start, double _meanw_at_start,
                    double _price_per_kg_at_start, double _target_meanw_at_harvest, int _nb_fish_at_harvest, double _meanw_at_harvest,
@@ -46,7 +47,26 @@ Fishfarm::Fishfarm(int _name, string _stringname, Node *_node, double _size, dou
                    double _market_price_sold_fish, double _operating_cost_per_days, double _annual_profit
                    )
     : name(_name), stringname(_stringname), x(_node->get_x()), y(_node->get_y()),
-      size(_size), farm_original_long(_farm_original_long), farm_original_lat(_farm_original_lat)
+      size(_size), farm_original_long(_farm_original_long), farm_original_lat(_farm_original_lat),
+      mean_SST(_mean_SST), mean_salinity(_mean_salinity), mean_windspeed(_mean_windspeed), mean_currentspeed(_mean_currentspeed), max_depth(_max_depth), diss_O2_mg_per_l(_diss_O2_mg_per_l),
+      Linf_mm(_Linf_mm), K_y(_K_y), t0_y(_t0_y), fulton_condition_factor(_fulton_condition_factor),
+      meanw_growth_model_type(_meanw_growth_model_type),
+      start_day_growing(_start_day_growing), end_day_harvest(_end_day_harvest), nb_days_fallowing_period(_nb_days_fallowing_period),
+      nb_fish_at_start(_nb_fish_at_start), meanw_at_start(_meanw_at_start),
+      price_per_kg_at_start(_price_per_kg_at_start), target_meanw_at_harvest(_target_meanw_at_harvest), nb_fish_at_harvest(_nb_fish_at_harvest), meanw_at_harvest(_meanw_at_harvest),
+      prop_harvest_kg_sold(_prop_harvest_kg_sold), kg_eggs_per_kg(_kg_eggs_per_kg), price_eggs_per_kg(_price_eggs_per_kg),
+      N_in_fish_kg_3per(_N_in_fish_kg_3per), P_in_fish_kg_0_5per(_P_in_fish_kg_0_5per),
+      feed_types(_feed_types), feed_price_per_kg(_feed_price_per_kg), total_feed_kg(_total_feed_kg), prop_N_in_feed(_prop_N_in_feed), prop_P_in_feed(_prop_P_in_feed),
+      total_feed_N_kg(_total_feed_N_kg), total_feed_P_kg(_total_feed_P_kg),
+      feed_type_vet(_feed_type_vet), feed_vet_price_per_kg(_feed_vet_price_per_kg), total_feed_vet_kg(_total_feed_vet_kg),
+      prop_N_in_feed_vets(_prop_N_in_feed_vets), prop_P_in_feed_vet(_prop_P_in_feed_vet),
+      total_feed_vet_N_kg(_total_feed_vet_N_kg), total_feed_vet_P_kg(_total_feed_vet_P_kg),
+      annual_discharge_N_kg(_annual_discharge_N_kg), annual_discharge_P_kg(_annual_discharge_P_kg),
+      annual_discharge_C_kg (_annual_discharge_C_kg), annual_discharge_heavymetals_kg(_annual_discharge_heavymetals_kg),
+      annual_discharge_medecine_kg (_annual_discharge_medecine_kg),  net_harvest_kg_per_sqkm_y(_net_harvest_kg_per_sqkm_y),
+      market_price_sold_fish(_market_price_sold_fish), operating_cost_per_days(_operating_cost_per_days), annual_profit(_annual_profit)
+
+
 {
  p_location_ff=_node;
 }
@@ -55,6 +75,14 @@ Fishfarm::~Fishfarm()
 {
     //dtor
 }
+
+
+
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * GETTERS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
 
 int Fishfarm::get_name() const
 {
@@ -97,6 +125,81 @@ double Fishfarm::get_y() const
     return(y);
 }
 
+
+double Fishfarm::get_Linf_mm() const
+{
+    return(Linf_mm);
+}
+
+double Fishfarm::get_K_y() const
+{
+    return(K_y);
+}
+
+double Fishfarm::get_t0_y() const
+{
+    return(t0_y);
+}
+
+double Fishfarm::get_fulton_condition_factor() const
+{
+    return(fulton_condition_factor);
+}
+
+string Fishfarm::get_meanw_growth_model_type() const
+{
+    return(meanw_growth_model_type);
+}
+
+double Fishfarm::get_sim_individual_mean_kg() const
+{
+    return(sim_individual_mean_kg);
+}
+
+double Fishfarm::get_sim_kg_harvested() const
+{
+    return(sim_kg_harvested);
+}
+
+double Fishfarm::get_sim_kg_eggs_harvested() const
+{
+    return(sim_kg_eggs_harvested);
+}
+
+double Fishfarm::get_sim_annual_profit() const
+{
+    return(sim_annual_profit);
+}
+
+double Fishfarm::get_sim_net_discharge_N() const
+{
+    return(sim_net_discharge_N);
+}
+
+double Fishfarm::get_sim_net_discharge_P() const
+{
+    return(sim_net_discharge_P);
+}
+
+double Fishfarm::get_sim_net_discharge_C() const
+{
+    return(sim_net_discharge_C);
+}
+
+double Fishfarm::get_sim_net_discharge_medecine() const
+{
+    return(sim_net_discharge_medecine);
+}
+
+
+
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * SETTERS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+
+
 void Fishfarm::set_x(double _x)
 {
     x= _x;
@@ -106,3 +209,67 @@ void Fishfarm::set_y(double _y)
 {
     y= _y;
 }
+
+
+
+void Fishfarm::set_sim_individual_mean_kg(double _value)
+{
+    sim_individual_mean_kg= _value;
+}
+void Fishfarm::set_sim_kg_harvested(double _value)
+{
+    sim_kg_harvested= _value;
+}
+
+void Fishfarm::set_sim_kg_eggs_harvested(double _value)
+{
+    sim_kg_eggs_harvested= _value;
+}
+
+void Fishfarm::set_sim_annual_profit(double _value)
+{
+    sim_annual_profit= _value;
+}
+
+void Fishfarm::set_sim_net_discharge_N(double _value)
+{
+    sim_net_discharge_N= _value;
+}
+
+void Fishfarm::set_sim_net_discharge_P(double _value)
+{
+    sim_net_discharge_P= _value;
+}
+
+void Fishfarm::set_sim_net_discharge_C(double _value)
+{
+    sim_net_discharge_C= _value;
+}
+
+void Fishfarm::set_sim_net_discharge_medecine(double _value)
+{
+    sim_net_discharge_medecine= _value;
+}
+
+
+void Fishfarm::compute_current_sim_individual_mean_kg_in_farm()
+{
+    ;
+}
+
+void Fishfarm::compute_profit_in_farm()
+{
+    ;
+}
+
+void Fishfarm::compute_discharge_on_farm()
+{
+    ;
+}
+
+
+
+
+
+
+
--- include/Fishfarm.h
+++ include/Fishfarm.h
@@ -60,16 +60,77 @@ class Fishfarm
         double get_farm_original_lat() const;
         double get_x() const;
         double get_y() const;
+
+        double get_Linf_mm() const; // variables for growth modelling
+        double get_K_y() const;
+        double get_t0_y() const;
+        double get_fulton_condition_factor() const;
+        string get_meanw_growth_model_type() const;
+
+        double get_sim_individual_mean_kg() const; // for tracking
+        double get_sim_kg_harvested() const;
+        double get_sim_kg_eggs_harvested() const;
+        double get_sim_annual_profit() const;
+        double get_sim_net_discharge_N() const;
+        double get_sim_net_discharge_P() const;
+        double get_sim_net_discharge_C() const;
+        double get_sim_net_discharge_medecine() const;
+
         void set_x(double value);
         void set_y(double value);
+
+        void set_sim_individual_mean_kg(double value);
+        void set_sim_kg_harvested(double value);
+        void set_sim_kg_eggs_harvested(double value);
+        void set_sim_annual_profit(double value);
+        void set_sim_net_discharge_N(double value);
+        void set_sim_net_discharge_P(double value);
+        void set_sim_net_discharge_C(double value);
+        void set_sim_net_discharge_medecine(double value);
+
+
+        void compute_current_sim_individual_mean_kg_in_farm();  // methods for growth modelling
+        void compute_profit_in_farm();  // methods for fishfarm economy
+        void compute_discharge_on_farm();  // methods for envirnmental impact on node
+
+
 
     protected:
     private:
         int name;
         string stringname;
         Node* p_location_ff;
+
         double farm_original_long, farm_original_lat;
         double x, y;
         double size;
+        double mean_SST, mean_salinity, mean_windspeed, mean_currentspeed, max_depth, diss_O2_mg_per_l;
+        double Linf_mm,  K_y, t0_y,  fulton_condition_factor;
+        string meanw_growth_model_type;
+        int start_day_growing, end_day_harvest, nb_days_fallowing_period, nb_fish_at_start, nb_fish_at_harvest;
+        double meanw_at_start, price_per_kg_at_start, target_meanw_at_harvest, meanw_at_harvest;
+        double prop_harvest_kg_sold, kg_eggs_per_kg, price_eggs_per_kg;
+        double N_in_fish_kg_3per, P_in_fish_kg_0_5per;
+        string feed_types;
+        double feed_price_per_kg, total_feed_kg, prop_N_in_feed, prop_P_in_feed;
+        double total_feed_N_kg, total_feed_P_kg;
+        string feed_type_vet;
+        double feed_vet_price_per_kg, total_feed_vet_kg, prop_N_in_feed_vets, prop_P_in_feed_vet;
+        double total_feed_vet_N_kg, total_feed_vet_P_kg;
+        double annual_discharge_N_kg, annual_discharge_P_kg;
+        double annual_discharge_C_kg, annual_discharge_heavymetals_kg;
+        double annual_discharge_medecine_kg, net_harvest_kg_per_sqkm_y;
+        double market_price_sold_fish, operating_cost_per_days, annual_profit;
+
+
+        // tracked data
+        double sim_individual_mean_kg;
+        double sim_kg_harvested;
+        double sim_kg_eggs_harvested;
+        double sim_annual_profit;
+        double sim_net_discharge_N, sim_net_discharge_P, sim_net_discharge_C, sim_net_discharge_medecine;
+
+
+
 };
 #endif							 // FISHFARM_H

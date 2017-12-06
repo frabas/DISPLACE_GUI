frabas/DISPLACE_GUI

commit 5a5ec73696a464c502105e3d642fe77735ace66b

v0.9.9.1 (for the Greeks)

include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.9.9"
+#define VERSION "0.9.9.1"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 0,9,9,VERSION_BUILD
+#define VERSION_COMMA 0,9,9,1,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "0.9.9\0"
+#define VER_PRODUCTVERSION_STR      "0.9.9.1\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"

setup.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Displace"
-#define MyAppVersion "0.9.9"
+#define MyAppVersion "0.9.9.1"
 #define MyAppPublisher "Displace Project"
 #define MyAppURL "http://www.displace-project.org"
 #define MyAppExeName "displacegui.exe"

simulator/main.cpp
@@ -4575,7 +4575,7 @@ const char *const path = "\"C:\\Program Files (x86)\\gnuplot\\bin\\gnuplot\"";
                         }
 
                         // remember to make sure size() of fgrounds == size() of freq_fgrounds
-                        ASSERT(fgrounds.size()==freq_fgrounds.size())
+                        //ASSERT(fgrounds.size()==freq_fgrounds.size())
 
                         // cout << "assume equal probas "  << endl;
                         // vector<double> equal_proba (fgrounds.size(), 1/fgrounds.size());

commit f71bedde51da23f29ded0d77a0199b66acd2c4d7

Fix compilation issue on windows introduced with commit 5a5ec736

include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.9.9.1"
+#define VERSION "0.9.10"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 0,9,9,1,VERSION_BUILD
+#define VERSION_COMMA 0,9,10,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "0.9.9.1\0"
+#define VER_PRODUCTVERSION_STR      "0.9.10\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"
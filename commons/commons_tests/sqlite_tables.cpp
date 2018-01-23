eab2cd7e1aca2154cae84b6b9b193295e0c0772f
Federico Fuga <fuga@studiofuga.com>
2018-01-23 22:53:17 +0100

    Added unit test for VmsLog table

--- commons/commons_tests/sqlite_tables.cpp
+++ commons/commons_tests/sqlite_tables.cpp
@@ -7,6 +7,7 @@
 #include "sqlitefielddef.h"
 
 #include "storage/tables/vesselsloglikecatchestable.h"
+#include "storage/tables/vesselvmsliketable.h"
 
 using namespace sqlite;
 
@@ -15,15 +16,25 @@ BOOST_AUTO_TEST_CASE (sqlite_tables)
     auto db = std::make_shared<SQLiteStorage>(":memory:");
     db->open();
 
-    auto table = std::make_shared<VesselsLoglikeCatchesTable>(db, "catches");
-    BOOST_NOEXCEPT(table->dropAndCreate());
+    {
+        auto table = std::make_shared<VesselsLoglikeCatchesTable>(db, "catches");
+        BOOST_NOEXCEPT(table->dropAndCreate());
 
-    std::vector<double> catches { 1.0, 2.0, 3.0 };
-    std::vector<double> discards { 1.0, 2.0, 3.0 };
+        std::vector<double> catches { 1.0, 2.0, 3.0 };
+        std::vector<double> discards { 1.0, 2.0, 3.0 };
 
-    BOOST_NOEXCEPT(table->insert(1, 0, catches, discards));
-    BOOST_NOEXCEPT(table->insert(1, 1, catches, discards));
-    BOOST_NOEXCEPT(table->insert(1, 2, catches, discards));
+        BOOST_NOEXCEPT(table->insert(1, 0, catches, discards));
+        BOOST_NOEXCEPT(table->insert(1, 1, catches, discards));
+        BOOST_NOEXCEPT(table->insert(1, 2, catches, discards));
+    }
+
+    {
+        auto table = std::make_shared<VesselVmsLikeTable>(db, "vms");
+        BOOST_NOEXCEPT(table->dropAndCreate());
+
+        VesselVmsLikeTable::Log l1 { 0, 0, 1,  1.0, 2.0, 180.0, 200, 1 };
+        BOOST_NOEXCEPT(table->insertLog(l1));
+    }
 }
 
 

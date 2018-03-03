fc7cb1908c1dfb43025b76e1a5ba8350ac4b6139
Francois Bastardie <fbas@win.dtu.dk>
2018-03-03 07:25:56 +0100

    Restore previous icon for now

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
 
 #define VERSION "0.9.11"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 0,9,10,VERSION_BUILD
+#define VERSION_COMMA 0,9,11,VERSION_BUILD
 
 #define VER_PRODUCTVERSION_STR      "0.9.11\0"
 
@@ -15,7 +15,7 @@
 #define VER_COMPANYNAME_STR         "Displace-project"
 #define VER_FILEDESCRIPTION_STR     "DisplaceGui"
 #define VER_INTERNALNAME_STR        "DisplaceGui.exe"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012, 2013, 2014, 2015, 2016, 2017 Francois Bastardie <fba@aqua.dtu.dk>"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012, 2013, 2014, 2015, 2016, 2017, 2018 Francois Bastardie <fba@aqua.dtu.dk>"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "displacegui.exe"

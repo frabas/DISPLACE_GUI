5380d55f0f8d7a73f9052fd4c0e74fe4ecd67cff
Federico Fuga <fuga@studiofuga.com>
2018-04-19 16:01:29 +0200

    Fix dependency by Qt for Commons and Formats libraries
    This should have broken HPC compilation somehow.

--- commons/commons_global.h
+++ commons/commons_global.h
@@ -1,12 +1,21 @@
 #ifndef COMMONS_GLOBAL_H
 #define COMMONS_GLOBAL_H
 
+#ifdef _WIN32
+
+// This is only relevant for Windows. Though commons.dll shouldn't depend by Qt, it is harmless because on Windows
+// we do require user to provide qt.
+
 #include <QtCore/qglobal.h>
 
 #if defined(COMMONS_LIBRARY)
 #  define COMMONSSHARED_EXPORT Q_DECL_EXPORT
 #else
 #  define COMMONSSHARED_EXPORT Q_DECL_IMPORT
+#endif
+#else
+
+#define COMMONSSHARED_EXPORT
 #endif
 
 

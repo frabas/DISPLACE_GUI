befb1927d084df6079bc351a8390a013c11c6e5d
Federico Fuga <fuga@studiofuga.com>
2018-04-20 18:13:40 +0200

    Fix Windows compilation (most of)

--- formats/formats_globals.h
+++ formats/formats_globals.h
@@ -5,7 +5,13 @@
 
 // This is only relevant for Windows. Though commons.dll shouldn't depend by Qt, it is harmless because on Windows
 // we do require user to provide qt.
-#include <QtCore/qglobal.h>
+#if defined(_WIN32) || defined(_WIN64)
+    #define Q_DECL_EXPORT __declspec(dllexport)
+    #define Q_DECL_IMPORT __declspec(dllimport)
+#else
+    #define Q_DECL_EXPORT
+    #define Q_DECL_IMPORT
+#endif
 
 #if defined(FORMATS_LIBRARY)
 #  define FORMATSSHARED_EXPORT Q_DECL_EXPORT

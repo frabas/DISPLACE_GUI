61cb34149ae4cca5fe83943734ce6b9f311d3735
Francois Bastardie <fba@aqua.dtu.dk>
2018-06-04 17:47:48 +0200

    A correct computation of discratio

--- commons/Vessel.cpp
+++ commons/Vessel.cpp
@@ -3169,7 +3169,8 @@ void Vessel::do_catch(ofstream& export_individual_tacs, vector<Population* >& po
     // contribute to accumulated catches on this node
     this->get_loc()->add_to_cumcatches(cumcatch_fgrounds.at(idx_node_r));
     this->get_loc()->add_to_cumdiscards(cumdiscard_fgrounds.at(idx_node_r));
-    double discratio = (cumdiscard_fgrounds.at(idx_node_r)+cumcatch_fgrounds.at(idx_node_r))>0 ? (cumdiscard_fgrounds.at(idx_node_r)/(cumdiscard_fgrounds.at(idx_node_r)+cumcatch_fgrounds.at(idx_node_r))) : 0.0;
+    double discratio =  this->get_loc()->get_cumdiscards() / (this->get_loc()->get_cumdiscards()+this->get_loc()->get_cumcatches());
+    discratio = discratio>0 ? discratio : 0.0;
     this->get_loc()->set_cumdiscardsratio(discratio);
 
 

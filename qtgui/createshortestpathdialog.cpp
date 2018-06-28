248c72334362c3f4288762290c388688964ad2da
ing. Federico Fuga <fuga@studiofuga.com>
2018-06-28 12:47:38 +0200

    Fix compilation under gcc7

--- qtgui/createshortestpathdialog.cpp
+++ qtgui/createshortestpathdialog.cpp
@@ -24,6 +24,7 @@
 #include <QFileDialog>
 #include <QSettings>
 #include <QMessageBox>
+#include <QIntValidator>
 
 CreateShortestPathDialog::CreateShortestPathDialog(QWidget *parent) :
     QDialog(parent),

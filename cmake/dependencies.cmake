d10709d61773234988905d1d4d6bdfc2e8825655
Federico Fuga <fuga@studiofuga.com>
2018-10-08 21:51:23 +0200

    cmake's CPack configuration with MacOS Bundle specificataions

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,8 +1,18 @@
 # Check dependencies
 
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/modules)
+
 ## Common packages to all modules
 find_package(Boost 1.55 REQUIRED)
 
 include_directories(${Boost_INCLUDE_DIRS})
 link_directories(${Boost_LIBRARY_DIRS})
 
+find_package(GeographicLib REQUIRED)
+message ("GeographicLib version: ${GeographicLib_VERSION}")
+
+find_package(GDAL REQUIRED 1.11)
+message ("GDAL library: ${GDAL_LIBRARY}")
+
+find_package(cgal REQUIRED)
+message ("cgal version: ${CGAL_VERSION}")

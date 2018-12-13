f95cef1d0e9edc438d4677254d7314ea994f16e7
ing. Federico Fuga <fuga@studiofuga.com>
2018-12-13 19:45:14 +0100

    Added paths for linux setup

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -9,13 +9,21 @@ link_directories( ${Boost_LIBRARY_DIRS} )
 
 find_package(GeographicLib REQUIRED)
 message ("GeographicLib version: ${GeographicLib_VERSION}")
+include_directories( ${GeographicLib_INCLUDE_DIR} )
+link_directories( ${GeographicLib_LIBRARY_DIRS} )
 
 find_package(GDAL REQUIRED 1.11)
 message ("GDAL library: ${GDAL_LIBRARY}")
+include_directories( ${GDAL_INCLUDE_DIR} )
+link_directories( ${GDAL_LIBRARY_DIRS} )
 
 find_package(CGAL REQUIRED)
 message ("CGAL version: ${CGAL_VERSION}")
+include_directories( ${CGAL_INCLUDE_DIR} )
+link_directories( ${CGAL_LIBRARY_DIRS} )
 
 find_package(Sparsepp REQUIRED)
 
 find_package(msqlitecpp REQUIRED)
+include_directories( ${sparsepp_INCLUDE_DIR} )
+link_directories( ${sparsepp_LIBRARY_DIRS} )

360df14bae70f23232f3145ec0e6dfd0df3d44b0
frabas <fba@aqua.dtu.dk>
2018-12-17 22:02:26 +0100

    Fix the shells for compiling on hpc

--- compile_msqlitecpp_on_hpc.sh
+++ compile_msqlitecpp_on_hpc.sh
@@ -11,8 +11,6 @@ cd Build
 
 /appl/cmake/3.12.2/bin/cmake .. -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="/appl/boost/1.64.0-gcc-6.3.0/" -DCMAKE_CXX_COMPILER="/appl/gcc/7.3.0/bin/g++" -DCMAKE_C_COMPILER="/appl/gcc/7.3.0/bin/gcc" -DENABLE_TEST=Off -DENABLE_PROFILER=Off
 
-make
-
-make DESTDIR="/zhome/fe/8/43283/ibm_vessels/local/mSqliteCpp/" install
+make DESTDIR="/zhome/fe/8/43283/local/mSqliteCpp/" install
 
 #-DENABLE_SQLITE_AMALGAMATION=ON -DSQLITE_SOURCE_PATH="/zhome/fe/8/43283/ibm_vessels_DISPLACE_GUI/commons/storage/" -DSQLITE_INCLUDE_PATH="zhome/fe/8/43283/ibm_vessels_DISPLACE_GUI/commons/storage/"

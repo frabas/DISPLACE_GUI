1b0021c08d7c2843d2e17dd91de990db4c6b465e
frabas <fba@aqua.dtu.dk>
2019-01-16 22:22:48 +0100

    Fix hpc compilation shell for geographiclib

--- compile_on_hpc.sh
+++ compile_on_hpc.sh
@@ -5,11 +5,9 @@ echo "Compiling DISPLACE with cmake..."
 
 cd Build
 
-#Dmsqlitecpp_DIR="/zhome/fe/8/43283/ibm_vessels/mSqliteCpp/cmake/"
 
-/appl/cmake/3.12.2/bin/cmake .. -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="/appl/boost/1.64.0-gcc-6.3.0/" -DCMAKE_CXX_COMPILER="/appl/gcc/7.3.0/bin/g++" -DCMAKE_C_COMPILER="/appl/gcc/7.3.0/bin/gcc" -DGEOGRAPHIC_INCLUDE="/zhome/fe/8/43283/local/geographiclib-1.49/include/" -DGEOGRAPHIC_LIBDIR="~//local/geographiclib-1.49/lib/" -DCMAKE_PREFIX_PATH="/zhome/fe/8/43283/local/mSqliteCpp/usr/local/lib64/cmake/msqlitecpp/" -DWITHOUT_GUI=On
+/appl/cmake/3.12.2/bin/cmake .. -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="/appl/boost/1.64.0-gcc-6.3.0/" -DCMAKE_CXX_COMPILER="/appl/gcc/7.3.0/bin/g++" -DCMAKE_C_COMPILER="/appl/gcc/7.3.0/bin/gcc" -DGeographicLib_LIBRARIES="~//local/geographiclib-1.49/" -DGeographicLib_INCLUDE_DIRS="~//local/geographiclib-1.49/include/" -DGeographicLib_LIBRARY_DIRS="~//local/geographiclib-1.49/lib/" -DCMAKE_MODULE_PATH="~//local/geographiclib-1.49/share/cmake/GeographicLib/" -DCMAKE_PREFIX_PATH="/zhome/fe/8/43283/local/mSqliteCpp/usr/local/lib64/cmake/msqlitecpp/" -DWITHOUT_GUI=On
 
-#-DMSQLITECPP_INCLUDE="/zhome/fe/8/43283/local/mSqliteCpp/usr/local/include/" -DMSQLITECPP_LIBDIR="/zhome/fe/8/43283/local/mSqliteCpp/usr/local/lib64/cmake/msqlitecpp"
       
 
 

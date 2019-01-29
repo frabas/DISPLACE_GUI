3d7be69895218f413512233743e4ce8397862399
ing. Federico Fuga <fuga@studiofuga.com>
2019-01-29 10:19:16 +0100

    Fix crash on loading db

--- qtgui/dbhelper.cpp
+++ qtgui/dbhelper.cpp
@@ -215,6 +215,8 @@ bool DbHelper::loadNodes(QList<std::shared_ptr<NodeData> > &nodes, QList<std::sh
     auto nodesTab = p->db->getNodesDefTable();
 
     nodesTab->queryAllNodes([&nodes, &model, &harbours](std::shared_ptr<Node> nodeData, std::shared_ptr<Harbour> harbour) {
+        nodeData->init_Ns_pops_at_szgroup(model->getNBPops(), model->getSzGrupsCount());
+
         auto n = std::make_shared<NodeData>(nodeData, model);
 
         auto idx = n->get_idx_node().toIndex();

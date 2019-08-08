e1bc028cad8eff3e4e4f5e8526589791c7a1f6c0
Federico Fuga <fuga@studiofuga.com>
2019-08-08 18:27:40 +0200

    Added other fields to nodes
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- simulator/data/NodesLoader.cpp
+++ simulator/data/NodesLoader.cpp
@@ -29,6 +29,18 @@ db::Column<db::ColumnTypes::Integer> fieldHIdx{"hidx"};
 db::Column<db::ColumnTypes::Integer> fieldGraphSce{"graphsce"};
 db::Column<db::ColumnTypes::Integer> fieldCodeArea{"code_area"};
 db::Column<db::ColumnTypes::Integer> fieldMarineLandscape{"landscape"};
+db::Column<db::ColumnTypes::Real> fieldWind{"wind"};
+db::Column<db::ColumnTypes::Real> fieldSalinity{"salinity"};
+db::Column<db::ColumnTypes::Real> fieldSst{"sst"};
+db::Column<db::ColumnTypes::Real> fieldNitrogen{"nitrogen"};
+db::Column<db::ColumnTypes::Real> fieldPhosphorus{"phosphorus"};
+db::Column<db::ColumnTypes::Real> fieldOxygen{"oxygen"};
+db::Column<db::ColumnTypes::Real> fieldCarbon{"carbon"};
+db::Column<db::ColumnTypes::Real> fieldBathymetry{"bathymetry"};
+db::Column<db::ColumnTypes::Real> fieldShipping{"shipping"};
+db::Column<db::ColumnTypes::Real> fieldSilt{"silt"};
+db::Column<db::ColumnTypes::Real> fieldBenthosBio{"benthosbio"};
+db::Column<db::ColumnTypes::Real> fieldBenthosNum{"benthosnum"};
 }
 
 NodesLoader::NodesLoader(std::string inputFolder, std::string folder_name_parameterization, int nrow)
@@ -48,12 +60,20 @@ std::vector<Node *> NodesLoader::load(int agraph)
 
     auto query = db::makeSelectStatement(p->db, NodesTableName,
                                          fieldId, fieldX, fieldY, fieldHIdx,
-                                         fieldCodeArea, fieldMarineLandscape
+                                         fieldCodeArea, fieldMarineLandscape,
+                                         fieldWind, fieldSalinity, fieldSst,
+                                         fieldNitrogen, fieldPhosphorus, fieldOxygen,
+                                         fieldCarbon,
+                                         fieldBathymetry,
+                                         fieldShipping, fieldSilt, fieldBenthosBio, fieldBenthosNum
     );
     query.where(fieldGraphSce == "agraph");
 
     query.bind(agraph);
-    query.execute([&nodes](int id, double x, double y, int hidx, int codeArea, int marineLandscape) {
+    query.execute([&nodes](int id, double x, double y, int hidx, int codeArea, int marineLandscape,
+                           double wind, double salinity, double sst, double ni, double ph, double ox, double ca,
+                           double bath, double ship, double silt, double bb, double bn
+    ) {
         auto node = new Node();
         node->set_idx_node(types::NodeId{static_cast<uint16_t>(id)});
         node->set_xy(x, y);
@@ -61,6 +81,19 @@ std::vector<Node *> NodesLoader::load(int agraph)
 
         node->setMarineLandscape(marineLandscape);
         node->setCodeArea(codeArea);
+
+        node->setWind(wind);
+        node->setSalinity(salinity);
+        node->setSST(sst);
+        node->setNitrogen(ni);
+        node->setPhosphorus(ph);
+        node->setOxygen(ox);
+        node->setDissolvedCarbon(ca);
+        node->setBathymetry(bath);
+        node->setShippingdensity(ship);
+        node->setSiltfraction(silt);
+        node->setBenthosBiomass(bb);
+        node->setBenthosNumber(bn);
 
         nodes.push_back(node);
         return true;

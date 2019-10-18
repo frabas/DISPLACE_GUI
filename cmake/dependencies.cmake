34f1f98330c94e92f00794284f467a769c2aef67
Federico Fuga <fuga@studiofuga.com>
2019-10-18 17:14:57 +0200

    Added error handling code to tests
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -3,7 +3,7 @@
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
 
 ## Common packages to all modules
-find_package(Boost 1.55 REQUIRED COMPONENTS date_time system thread program_options log unit_test_framework)
+find_package(Boost 1.55 REQUIRED COMPONENTS date_time filesystem system thread program_options log unit_test_framework)
 include_directories(${Boost_INCLUDE_DIR})
 link_directories(${Boost_LIBRARY_DIRS})
 

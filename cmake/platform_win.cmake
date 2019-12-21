e56ae1ec0acf868e288babf2d7c59fd96ff37942
Federico Fuga <fuga@studiofuga.com>
2019-12-21 11:55:22 +0100

    Windows: removed warning C4275
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- cmake/platform_win.cmake
+++ cmake/platform_win.cmake
@@ -16,6 +16,10 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")     # Visual studio
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP /permissive-")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /MP")
 
+    # Fix warning C4275
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -wd4275")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /MP -wd4275")
+
     # Fix visual studio Bug with boo: error C2116: 'boost::interprocess::winapi::CreateDirectoryA'
     add_definitions(-DBOOST_USE_WINDOWS_H -DNOMINMAX)
 

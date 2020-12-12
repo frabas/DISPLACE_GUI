ebcc0755f6056a8433896710b60c80134de66180
Federico Fuga <fuga@studiofuga.com>
2020-12-12 16:49:54 +0100

    Fix accessing vectors
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- commons/DatabaseInputImpl/dispatchers/PopGroupsFunctions.cpp
+++ commons/DatabaseInputImpl/dispatchers/PopGroupsFunctions.cpp
@@ -4,61 +4,70 @@
 
 #include "PopGroupsFunctions.h"
 
+template<typename V>
+V &at(std::vector<V> &v, int index)
+{
+    while (v.size() <= index) {
+        v.emplace_back(V{});
+    }
+    return v.at(index);
+}
+
 void
 fillInitComcat(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_comcat_at_szgroup[group] = value;
+    at(population.init_comcat_at_szgroup, group) = value;
 }
 
 void fillInitPopN(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_tot_N_at_szgroup[group] = value;
+    at(population.init_tot_N_at_szgroup, group) = value;
 }
 
 void fillInitM(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_M_at_szgroup[group] = value;
+    at(population.init_M_at_szgroup, group) = value;
 }
 
 void fillInitFec(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_fecundity_at_szgroup[group] = value;
+    at(population.init_fecundity_at_szgroup, group) = value;
 }
 
 void
 fillInitWeight(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_weight_at_szgroup[group] = value;
+    at(population.init_weight_at_szgroup, group) = value;
 }
 
 void
 fillInitMaturity(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_maturity_at_szgroup[group] = value;
+    at(population.init_maturity_at_szgroup, group) = value;
 }
 
 void
 fillInitPropRecru(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_proprecru_at_szgroup[group] = value;
+    at(population.init_proprecru_at_szgroup, group) = value;
 }
 
 void fillPercentAgePerSzGroup(PopulationsLoader::PopulationData &population, int group, int age, int node, int period,
                               double value)
 {
     // TODO check the ordering of the indexes!
-    population.percent_age_per_szgroup_matrix[age][group] = value;
+    at(at(population.percent_age_per_szgroup_matrix, age), group) = value;
 }
 
 void fillPercentSzGroupPerAge(PopulationsLoader::PopulationData &population, int group, int age, int node, int period,
                               double value)
 {
     // TODO check the ordering of the indexes!
-    population.percent_szgroup_per_age_matrix[group][age] = value;
+    at(at(population.percent_szgroup_per_age_matrix, group), age) = value;
 }
 
 void
 fillPropMigrants(PopulationsLoader::PopulationData &population, int group, int age, int node, int period, double value)
 {
-    population.init_prop_migrants_in_tot_N_at_szgroup[group] = value;
+    at(population.init_prop_migrants_in_tot_N_at_szgroup, group) = value;
 }

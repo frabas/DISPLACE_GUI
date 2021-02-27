ca697a882da216a13e146b9ef89f6549aae57667
Federico Fuga <fuga@studiofuga.com>
2021-02-27 12:25:13 +0100

    Tests: Include the correct boost header
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- commons/commons_tests/inputfiles_fishfarmsspe.cpp
+++ commons/commons_tests/inputfiles_fishfarmsspe.cpp
@@ -6,11 +6,12 @@
  * */
 
 #define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
-#include <boost/test/floating_point_comparison.hpp>
+#include "myutils.h"
+#include "readdata.h"
 
-#include <myutils.h>
-#include <readdata.h>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
+
 #include <map>
 
 BOOST_AUTO_TEST_SUITE ( fishfarmsspe )

dea368e086b04f5a60e1c56e22fd57efd1ebc188
Francois Bastardie <fbas@dtu.dk>
2024-10-23 10:44:27 +0200

    v 1.2.3

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "1.2.2"
+#define VERSION "1.2.3"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 1,2,2,VERSION_BUILD
+#define VERSION_COMMA 1,2,3,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "1.2.2\0"
+#define VER_PRODUCTVERSION_STR      "1.2.3\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"

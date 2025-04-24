eb5d289cc854cd1538a8cb5bb1ff16c6fdcc93e4
Francois Bastardie <fbas@dtu.dk>
2025-04-24 08:40:30 +0200

    v1.2.5

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "1.2.4"
+#define VERSION "1.2.5"
 #define VERSION_BUILD 0
-#define VERSION_COMMA 1,2,4,VERSION_BUILD
+#define VERSION_COMMA 1,2,5,VERSION_BUILD
 
-#define VER_PRODUCTVERSION_STR      "1.2.4\0"
+#define VER_PRODUCTVERSION_STR      "1.2.5\0"
 
 #define VER_FILEVERSION             VERSION_COMMA
 #define VER_FILEVERSION_STR         VERSION "\0"

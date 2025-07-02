22352211330291203d651571832250cbc7d1cfcf
Federico Fuga <fuga@studiofuga.com>
2025-07-02 13:07:14 +0200

    Fixes for compiling on new ms vc compiler

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -6,3 +6,6 @@ set(CMAKE_CXX_STANDARD 14)
 if (CMAKE_BUILD_TYPE MATCHES Debug)
     add_definitions(-DDEBUG)
 endif ()
+
+# Hack for silly Windows SDK/Qt6 bug generating error C2872> byte ambiguous symbol
+add_definitions(-D_HAS_STD_BYTE=0)

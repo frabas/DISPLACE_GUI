2de8faf1052d7028ee9d68e0c99eaa7ee925591f
Federico Fuga <fuga@studiofuga.com>
2025-07-22 21:41:29 +0200

    Adjust compiler macros to build on Qt6 and msvc22

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -6,3 +6,7 @@ set(CMAKE_CXX_STANDARD 14)
 if (CMAKE_BUILD_TYPE MATCHES Debug)
     add_definitions(-DDEBUG)
 endif ()
+
+if (WIN32)
+    add_definitions(-DWIN32_LEAN_AND_MEAN)
+endif ()
--- cmake/platform_win.cmake
+++ cmake/platform_win.cmake
@@ -5,7 +5,7 @@ endif()
 
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS on)
 
-add_definitions(-D_WINSOCKAPI_)
+#add_definitions(-D_WINSOCKAPI_)
 
 if (CMAKE_BUILD_TYPE MATCHES Debug)
     add_definitions(-D_HAS_ITERATOR_DEBUGGING=1 -D_ITERATOR_DEBUG_LEVEL=2 -DDEBUG)
@@ -21,7 +21,7 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")     # Visual studio
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /MP -wd4275")
 
     # Fix visual studio Bug with boo: error C2116: 'boost::interprocess::winapi::CreateDirectoryA'
-    add_definitions(-DBOOST_USE_WINDOWS_H -DNOMINMAX)
+    add_definitions(-DNOMINMAX)
 
     message("MSVC: ${CMAKE_CXX_FLAGS}")
 endif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
